bfb4c9b631d7048a227661201f5c6369c1e63283
Chyngyz <camphosis@gmail.com>
2016-06-10 14:46:07 +0600

    Animated BB-8

---
+++ README.md
@@ -0,0 +1,5 @@
+## Angular Animated BB-8
+
+An Angular based interactive animation of BB-8 from Star Wars, exact clone recreated from React based one http://codepen.io/bullerb/pen/gMpxNZ.
+
+Styling and motion credits goes to http://codepen.io/bullerb, author of original pen http://codepen.io/bullerb/pen/gMpxNZ.
--- angular-bb8.js
+++ angular-bb8.js
@@ -1,14 +1,64 @@
+//
+// Angular Animated BB-8
+// http://codepen.io/Chyngyz/pen/YWwYGq
+//
+
+
 var app = angular.module('app', []);
 
-app.controller('mainCtrl', function() {
-  this.config = {
-    speed: 4,
-    accelMod: 1.2
+app.component('bb8App', {
+  templateUrl: 'bb8-app.html',
+  controller: function($interval, $document) {
+    var vm = this;
+    vm.config = {
+      droidX: 0,
+      mouseX: 0,
+      toTheRight: true,
+      speed: 2,
+      accelMod: 1.7
+    };
+
+    vm.handleMouseMove = function($event) {
+      vm.config.mouseX = $event.pageX;
+    };
+
+    vm.movement = function() {
+      if(Math.abs(Math.round(vm.config.droidX)-vm.config.mouseX) !== 1){
+
+          var distance = vm.config.mouseX - vm.config.droidX;
+          var acceleration = Math.abs(distance * vm.config.accelMod) / 100;
+
+          // Move to the right
+          if (vm.config.droidX < vm.config.mouseX) {
+              vm.config.droidX = vm.config.droidX+(vm.config.speed*acceleration);
+              vm.config.toTheRight = true;
+          }
+          // Move to the left
+          else {
+            vm.config.droidX = vm.config.droidX-(vm.config.speed*acceleration);
+            vm.config.toTheRight = false;
+          }
+      }
+    };
+
+    vm.$onInit = function() {
+      vm.config.mouseX = 300;
+      $document.on('mousemove', vm.handleMouseMove);
+    }
+
+    vm.$onDestroy = function() {
+      $document.off('mousemove', vm.handleMouseMove);
+    }
+
+    $interval(vm.movement, 1);
   }
-});
+})
 
 app.component('bb8', {
-  template: "<h2>This is to be bb8</h2>"
+  bindings: {
+    configData: "="
+  },
+  templateUrl: "bb8.html"
 });
 
 app.component('configBar', {
@@ -18,6 +68,4 @@ app.component('configBar', {
   templateUrl: 'configBar.html'
 })
 
-angular.bootstrap(document, ['app'], {
-  strictDi: true
-});
+angular.bootstrap(document, ['app']);
